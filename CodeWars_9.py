366fe58acd5971ae994ea2db66c4518368843b01
twinown <niyaz9797@gmail.com>
2025-02-24 01:57:14 +0300

    9

---
+++ CodeWars_9.py
@@ -0,0 +1,6 @@
+def leo(oscar):
+    return "Leo finally won the oscar! Leo is happy" if oscar ==88 else\
+             "Not even for Wolf of wallstreet?!" if oscar == 86 else\
+                "When will you give Leo an Oscar?" if oscar < 88 else "Leo got one already!"
+
+print(leo(86))
